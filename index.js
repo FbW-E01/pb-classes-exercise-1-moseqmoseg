9cba8f8ab21b4851bbea7f27eea61ad02bdfb337
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-05-18 13:44:19 +0200

    Fixed

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ class Employee {
     constructor(firstname, lastname) {
         this.firstname = firstname;
         this.lastname = lastname;
+        this.fullname = `${this.firstname} ${this.lastname}`;
+        this.email = `${this.firstname}.${this.lastname}@company.com`.toLowerCase();
         // Complete the code!
-    }
-
-    fullname() {
-        console.log(`${this.firstname} ${this.lastname}`);
-    }
-    email() {
-        console.log(`${this.firstname}.${this.lastname}@company.com`.toLowerCase());
     }
 
 
@@ -19,6 +14,8 @@ const emp1 = new Employee("John", "Smith");
 const emp2 = new Employee("Mary", "Sue");
 const emp3 = new Employee("Antony", "Walker");
 
-emp1.fullname();
-emp2.email();
+console.log(emp1.fullname);
+
+console.log(emp2.email);
+
 console.log(emp3.firstname);
