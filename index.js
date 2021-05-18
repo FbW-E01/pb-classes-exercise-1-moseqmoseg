2ad657e9ea93ca2c73e561b1b286fbb91a9302fd
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-05-18 13:36:14 +0200

    Completed

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 class Employee {
-	constructor (firstname, lastname) {
-		this.firstname = firstname
-		this.lastname = lastname
-		// Complete the code!
-	}
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        // Complete the code!
+    }
+
+    fullname() {
+        console.log(`${this.firstname} ${this.lastname}`);
+    }
+    email() {
+        console.log(`${this.firstname}.${this.lastname}@company.com`.toLowerCase());
+    }
+
+
 }
+
+const emp1 = new Employee("John", "Smith");
+const emp2 = new Employee("Mary", "Sue");
+const emp3 = new Employee("Antony", "Walker");
+
+emp1.fullname();
+emp2.email();
+console.log(emp3.firstname);
